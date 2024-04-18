5774d19bd85bbccd89091be3fc0e447477211d9a
Sergey Pomazan <86160616+yoonost@users.noreply.github.com>
2024-04-18 22:40:16 +0300

    18.04.2024

--- src/config.template.ts
+++ src/config.template.ts
@@ -1,5 +1,5 @@
-const apiId = 0
-const apiHash = ""
-const stringSession = ""
-const aezaSession = ""
+const apiId = 0 // Это ваш идентификатор, вы должны его получить были на сайте my.telegram.org
+const apiHash = "" // Это ваш хэш, вы должны его получить были на сайте my.telegram.org
+const stringSession = "" // Эту строчку мы на этапе установки не будем трогать
+const aezaSession = "" // Ваша сессия на AEZA.NET
 export { apiId, apiHash, stringSession, aezaSession }
